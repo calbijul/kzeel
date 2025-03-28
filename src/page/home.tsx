2974a9fcc0021fbbc60b1da79d37677df5bb759f
calbiju <calbijul@gmail.com>
2025-03-28 23:40:38 +1300

    initial commit after pull on home setup

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -4,7 +4,7 @@ function home() {
   return (
     <div className='bg-gray-900 w-full h-screen flex justify-center items-center'>
       <div className=''>
-      <h1 className='text-white'>WADUP</h1>
+      <h1 className='text-white tracking-widest'>WADUP</h1>
       </div>
     </div>
   )
